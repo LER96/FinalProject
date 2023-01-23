0317c1d6eb8c3a877cacbc434cfc0a93868fe28c
Liron Rotman <93140643+LER96@users.noreply.github.com>
2023-01-23 10:32:57 +0200

    Update NewUnit(master)
    All set and ready to go

--- FinalProject/NewUnit.cs
+++ FinalProject/NewUnit.cs
@@ -6,8 +6,24 @@ using System.Threading.Tasks;
 
 namespace FinalProject
 {
-    internal class NewUnit
+    internal class NewUnit : Unit
     {
+        public NewUnit()
+        {
 
+        }
+
+
+        public override void OnTileEnter(Tile tile)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnTileExit(Tile tile)
+        {
+            throw new NotImplementedException();
+        }
+
+        public delegate void Move(int x, int y);
     }
 }

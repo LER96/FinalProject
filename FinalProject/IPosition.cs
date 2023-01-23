53f7ace7080ea40b53fa03413d4f1adf2ef7ede0
Liron Rotman <93140643+LER96@users.noreply.github.com>
2023-01-23 10:24:51 +0200

    fixing IPosition(master)

--- FinalProject/IPosition.cs
+++ FinalProject/IPosition.cs
@@ -1,13 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FinalProject
 {
-    interface IPosition
+    public interface IPosition
     {
+        int X { get; }
+        int Y { get; }
+    }
+    readonly struct Position : IPosition, IEquatable<Position>
+    {
+        public int X { get; }
+        public int Y { get; }
 
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"({X}, {Y})";
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(X, Y);
+        }
+
+        //public override bool Equals(object obj)
+        //{
+        //    return obj is Position position && Equals(position);
+        //}
+
+        public bool Equals(Position other)
+        {
+            return X == other.X &&
+                   Y == other.Y;
+        }
+
+        public static Position operator +(Position a, Position b)
+        {
+            return new Position(a.X + b.X, a.Y + b.Y);
+        }
+
+        public static Position operator -(Position a, Position b)
+        {
+            return new Position(a.X - b.X, a.Y - b.Y);
+        }
     }
 }
